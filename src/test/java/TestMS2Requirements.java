2c6722d906658763e812011b877d72808be4588b
oguzp1 <oguz_paksoy_@hotmail.com>
2021-02-24 01:15:41 -0800

    added automated testing for ms2 requirements

---
+++ src/test/java/TestMS2Requirements.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+
+public class TestMS2Requirements extends BaseTest {
+    @Test
+    public void testRequirement1() {
+        testQuery("MS2Requirement1");
+    }
+
+    @Test
+    public void testRequirement2() {
+        testQuery("MS2Requirement2");
+    }
+
+    @Test
+    public void testRequirement3() {
+        testQuery("MS2Requirement3");
+    }
+
+    @Test
+    public void testRequirement4() {
+        testQuery("MS2Requirement4");
+    }
+
+    @Test
+    public void testRequirement5() {
+        testQuery("MS2Requirement5");
+    }
+}
