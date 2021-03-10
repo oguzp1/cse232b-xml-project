52490659c20747e3ece932a5c9820a1e3e408907
oguzp1 <oguz_paksoy_@hotmail.com>
2021-03-09 21:45:02 -0800

    added end to end tests

--- src/test/java/TestMS3Examples.java
+++ src/test/java/TestMS3Examples.java
@@ -30,4 +30,27 @@ public class TestMS3Examples extends BaseTest {
     public void testExplicitJoin3() {
         testQuery("MS3ExplicitJoin3");
     }
+
+    @Test
+    public void testExplicitJoin4() {
+        testQuery("MS3ExplicitJoin4");
+    }
+
+    @Test
+    public void testEndToEndJoin1() {
+        testRewrite("MS3EndToEndJoin1");
+        testQuery("MS3EndToEndJoin1");
+    }
+
+    @Test
+    public void testEndToEndJoin2() {
+        testRewrite("MS3EndToEndJoin2");
+        testQuery("MS3EndToEndJoin2");
+    }
+
+    @Test
+    public void testEndToEndJoin3() {
+        testRewrite("MS3EndToEndJoin3");
+        testQuery("MS3EndToEndJoin3");
+    }
 }
